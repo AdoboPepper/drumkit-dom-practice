052aab39d2c3974a91e0e703171f5fe3e7161ade
Jasper Calimbo <113905995+AdoboPepper@users.noreply.github.com>
2025-07-12 17:01:40 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+
+var length = document.querySelectorAll("button").length ;
+
+
+for (var i = 0; i < 7; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+
+
+function handleClick() {
+    var keyPressed = this.textContent;
+
+    playAudio(keyPressed);
+    buttonAnimatoin(keyPressed);
+   
+}
+
+function handlePress(keyPressed){
+
+    playAudio(keyPressed);
+    buttonAnimatoin(keyPressed);
+}
+
+function playAudio(keyPressed){
+     switch (keyPressed) {
+        case 'w':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default:
+            console.log("Unrecognized key: " + keyPressed);
+            break;
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    handlePress(event.key);
+})
+
+function buttonAnimatoin(keyPressed){
+    var activeButton = document.querySelector("."+keyPressed);
+    
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+
+
